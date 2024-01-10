44545bdd3a0b24c68d513e4bbb08522660779619
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-10 23:08:18 +0100

    [Update] Adding RedactingFormatter class with a method to format the log_record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -10,5 +11,27 @@ def filter_datum(fields: List[str], redaction: str,
     """Return the log message obfuscated"""
     for field in fields:
         message = re.sub("{}=.*?{}".format(field, separator),
-                         "{}={}{}".format(field, redaction, separator), message)
+                         " {}={}{}".format(field, redaction, separator),
+                         message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """ Init method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter value in incomming log records"""
+
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
