da35382d7f69927a9b9fca6691e6de19b118e920
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-16 10:16:43 +0100

    [Update] update the require_auth to check excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,11 @@ class Auth():
         Return:
           - Boolean
         """
-        return False
+        if not path or not excluded_paths:
+            return True
+        if path in excluded_paths or (path + '/') in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method used to create the authentication header
