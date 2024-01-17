1a038984dd1cbca6f0ce3d0ee34355e7ec440230
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-17 14:35:54 +0100

    [Update] Adding a method to return the cookies value

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -39,3 +40,11 @@ class Auth():
           - TypeVar('User')
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Return a cookie value from a request
+        """
+        if not request:
+            return None
+        session_name = os.getenv("SESSION_NAME")
+        return request.cookies.get("_my_session_id")
