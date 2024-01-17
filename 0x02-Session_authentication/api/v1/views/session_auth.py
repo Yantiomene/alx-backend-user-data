ecd9e77c54d1519b5a2309708bd66623faa727e4
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-17 17:49:47 +0100

    [Feature] Adding a session auth view

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" view to handle all routes for the Session authentication
+"""
+from api.v1.auth.session_auth import SessionAuth
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route("/auth_session/login", methods=["POST"],
+                 strict_slashes=False)
+def login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - The JSON
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email:
+        return jsonify({"error": "email is missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
