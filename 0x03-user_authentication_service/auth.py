2157c4e95d59bfbd30200f1d342ae23ef91db661
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 00:02:55 +0100

    [Feature] Adding a method to create a session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,14 @@ class Auth:
             return False
 
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session for a given user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        
+        self._db.update_user(user_id=user.id, session_id=_generate_uuid())
+        return user.session_id
