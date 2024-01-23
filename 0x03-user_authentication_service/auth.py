2b90376bafecfb5ea06bb4f875ab965115139f4a
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-23 21:49:43 +0100

    [Feature] Adding a method to register a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -10,3 +13,23 @@ def _hash_password(password: str) -> str:
     """
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pass = _hash_password(password)
+            user = self._db.add_user(email, hashed_pass)
+            return user
+        else:
+            raise ValueError("User {} already exists".format(email))
