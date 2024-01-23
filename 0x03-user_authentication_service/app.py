dede9f76296ceb7ba91214083ae91d9092f61894
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-23 23:09:54 +0100

    [Fix] Fixing requirement

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,9 @@
 """Basic flask app
 """
 
+
 from flask import Flask, jsonify, request
 from auth import Auth
-
 
 AUTH = Auth()
 
@@ -12,16 +12,16 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def index():
+def index() -> str:
     """index route
     """
     return jsonify({"message": "Bienvenue"})
 
 
-"""@app.route('/users', methods=['POST'])
-def users():
-    """'Posts a new user'
-"""
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Posts a new user
+    """
     email = request.form.get('email')
     password = request.form.get('password')
     try:
@@ -29,7 +29,6 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
-"""
 
 
 if __name__ == "__main__":
