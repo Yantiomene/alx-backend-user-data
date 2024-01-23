5b1b2ba7a69b3369c453aea2fc93711668037f35
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-23 23:45:33 +0100

    [feature] Adding a method to generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> str:
@@ -13,6 +14,11 @@ def _hash_password(password: str) -> str:
     """
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    """ Generate a string repr of a new UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
