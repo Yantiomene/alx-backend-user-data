2a78f0c05d2e12c43dd43fb4a3fbee97a0dfdd38
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 11:43:17 +0100

    [Feature] Adding a method to update the reset_token field

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,3 +83,13 @@ class Auth:
             return None
         self._db.update_user(user_id=user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate the UUID password reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(user.id, reset_token=_generate_uuid())
+        return user.reset_token
