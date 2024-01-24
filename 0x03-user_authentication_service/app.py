9d3e7a771c567d7ff9c3d73ff72cbcda2d7d3285
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 11:57:44 +0100

    [Feature] Adding a route to get the reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,5 +71,17 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """ Generate a token
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
