f4cb4fce0ece01dbde71716b5761690a519dc44e
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 12:16:08 +0100

    [Feature] Adding a method to update the password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,3 +93,17 @@ class Auth:
             raise ValueError
         self._db.update_user(user.id, reset_token=_generate_uuid())
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update the password using the reset token
+        """
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_pass = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_pass,
+                             reset_token=None)
+        return None
