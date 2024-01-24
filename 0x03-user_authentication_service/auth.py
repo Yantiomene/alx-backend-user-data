68df078624c87d3f72ff1d8c14c8efdff24109d6
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 10:21:59 +0100

    [Feature] Adding a method to destroy a session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,13 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy a user session id
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        self._db.update_user(user_id=user_id, session_id=None)
+        return None
