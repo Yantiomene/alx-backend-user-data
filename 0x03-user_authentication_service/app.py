2094a2f2401de08387b3ce7effe6218d10fbdf26
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-24 11:02:01 +0100

    [Feature] Adding a method to get the user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,12 +52,23 @@ def login() -> str:
 def logout() -> str:
     """ Logout a user
     """
-    session_id = request.cookie.get('session_id')
+    session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """ Get the user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
